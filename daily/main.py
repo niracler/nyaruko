fb0ed01cca9a2f548d74a5774644a5d4a5070793
niracler <i@niracler.com>
2023-08-14 23:58:45 +0800

    feat: Updated daily entry and added upcoming article list
    - Updated the daily entry for the Water Temple, expressing surprise at the hidden meaning behind the drop of water.
    - Added a list of upcoming articles for the next two months, with a note that there may be changes.
    - Included a mix of tech-related and entertainment articles, mostly focused on water themes.
    - Added English translations for the article titles and descriptions.
    - Tagged the first daily entry with "王国之泪" (Kingdom of Tears).
    
    Note: Please feel free to modify the commit message as needed.

---
+++ daily/main.py
@@ -0,0 +1,41 @@
+import configparser
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+from trello import TrelloClient
+
+trello_client = TrelloClient(
+    api_key=config["defaut"]["api_key"],
+    api_secret=config["defaut"]["api_secret"],
+    token=config["defaut"]["token"]
+)
+all_boards = trello_client.list_boards()
+last_board = all_boards[0]
+print(last_board.name)
+print(all_boards[0].list_lists()[4].name)
+print(all_boards[0].list_lists()[4].id)
+
+import yaml
+
+with open("docs/2023/08-3.yml") as f:
+    yml_str = f.read()
+data = yaml.safe_load(yml_str)
+
+card_id = data.get('random', {})['id']
+comment = data.get('daily', {})
+
+print(comment[-1]['cn'])
+
+# comment to card id
+
+card = trello_client.get_card(card_id)
+print(card.name)
+card.comment(comment[-1]['cn'])
+
+print("done")
+
+
+
+
+
+
