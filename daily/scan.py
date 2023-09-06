8f575449c1ef52a2ee92d7c97795cebfca7e8474
padpar <litinghui@x-cmd.com>
2023-09-06 19:40:11 +0800

    docs: list_articles_with_tag

--- daily/scan.py
+++ daily/scan.py
@@ -2,20 +2,22 @@
 statistic the articles in the folder
 """
 import os
+import re
 import click
+import yaml
 
 skip_folder = [
     'backup',
     'media',
     '.obsidian',
+    'templates',
 ]
 
 BASE_PATH = '/Users/niracler/iCloud云盘（归档）/Obsidian/Note'
 
-
-def list_markdown_files(folder_path: str, level=0) -> int:
+def list_article_as_tree(folder_path: str, level=0) -> int:
     """
-    recursively list all markdown files in the folder
+    recursively list all the articles in the folder as tree
     """
     if os.path.basename(folder_path) in skip_folder:
         return 0
@@ -32,7 +34,7 @@ def list_markdown_files(folder_path: str, level=0) -> int:
             count += 1
 
         elif os.path.isdir(file_path):
-            count += list_markdown_files(file_path, level + 1)
+            count += list_article_as_tree(file_path, level + 1)
 
     if count > 0:
         print(f"{level * '│  '}└── ({count})")
@@ -48,8 +50,72 @@ def cli():
     pass
 
 @cli.command("list")
-def list_articles():
+@click.option("--tree", "-t", is_flag=True, help="list the articles as tree")
+@click.option("--tag", default="", help="list the articles with tag")
+def list_articles(tree: bool, tag: str):
     """
     list the articles
     """
-    list_markdown_files(BASE_PATH, level=0)
+    if tree:
+        list_article_as_tree(BASE_PATH, level=0)
+        return
+
+    file_headers = list_articles_with_tag(BASE_PATH, tag)
+    # 打印提取的信息
+    # for header in file_headers:
+    #     print("Date:", header['date'])
+    #     print("Tags:", header['tags'])
+    #     print("---")
+
+
+# TODO: list the articles with tag
+# example:
+# ---
+# date: 2022-05-23
+# tags:
+#   - weekly1
+#   - weekly2
+#   - weekly3
+# ---
+# 定义一个函数来扫描文件头部信息
+def list_articles_with_tag(base_path, tag:str):
+    """
+    list the articles with tag
+    """
+    header_pattern = re.compile(r'^---\n(.*?)\n---', re.DOTALL)
+    headers = []
+
+    for root, dirs, files in os.walk(base_path, topdown=True):
+        # 检查是否需要跳过当前目录
+        dirs[:] = [d for d in dirs if d not in skip_folder]
+
+        for filename in files:
+            file_path = os.path.join(root, filename)
+
+            if not file_path.endswith('.md'):
+                continue    # 跳过非 Markdown 文件
+
+            with open(file_path, 'r', encoding='utf-8') as file:
+                file_contents = file.read()
+
+                match = header_pattern.search(file_contents)
+                header_data = {
+                    'date': 'N/A',
+                    'tags': 'N/A',
+                    'path': 'N/A'
+                }
+
+                if match:
+                    header_content = match.group(1)
+
+                    try:
+                        header_object = yaml.load(header_content, Loader=yaml.FullLoader)
+                        # print("Parsed Header Object:")
+                        # print(header_object)
+                    except yaml.YAMLError as error:
+                        print("path:", file_path)
+                        print("Error parsing YAML:", error)
+
+                headers.append(header_data)
+
+    return headers
